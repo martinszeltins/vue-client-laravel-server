904a1bf00de1b1f7b991a77e99e1d3a3fb289ca6
Martins <martinszeltins@gmail.com>
2020-10-20 16:12:30 +0300

    Client app

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# App with Vue.js on the front end and Laravel on the back end
+App with Vue.js on the front end and Laravel on the back end
---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    My app
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    html, body {
+        background: #202125;
+        color: #898a8d;
+    }
+</style>
